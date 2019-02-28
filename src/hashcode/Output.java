6a78d0898433a6d158491f284b08e9bc06c4ff26
kevin <kevinlackmann.kl@gmail.com>
2019-02-28 19:27:28 +0100

    output toString methods

--- src/hashcode/Output.java
+++ src/hashcode/Output.java
@@ -1,10 +1,7 @@
 package hashcode;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public class Output {
@@ -12,6 +9,7 @@ public class Output {
     public static <T> void writeOutput (List<T> objs, String filename) {
         try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(new File(filename))))) {
+            bw.write(objs.size() + "\n");
             final String output = objs.stream().map(Object::toString).collect(Collectors.joining("\n"));
             bw.write(output);
         } catch (IOException e) {
