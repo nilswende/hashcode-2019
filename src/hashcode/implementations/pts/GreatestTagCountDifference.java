cab848b6756d7afd9ca3fa30be84e0715c796be9
nilswende <s142075@student.dhbw-mannheim.de>
2019-02-28 20:30:36 +0100

    pts impl

--- src/hashcode/implementations/pts/GreatestTagCountDifference.java
+++ src/hashcode/implementations/pts/GreatestTagCountDifference.java
@@ -18,15 +18,17 @@ public class GreatestTagCountDifference implements PhotoToSlide {
     @Override
     public List<Slide> make (List<Photo> photos) {
         final List<Slide> slides = new ArrayList<>();
-        List<Photo> verticals = photos.stream().filter(Photo::isVertical).sorted(Comparator.comparingInt(a -> a.getTags().size())).collect(Collectors.toList());
-        List<Photo> horizontals = photos.stream().filter(Photo::isHorizontal).collect(Collectors.toList());
 
+        List<Photo> verticals = photos.stream().filter(Photo::isVertical).sorted(Comparator.comparingInt(a -> a.getTags().size())).collect(Collectors.toList());
         final ArrayDeque<Photo> deque = new ArrayDeque<>(verticals);
         for (int i = 0; i < deque.size() / 2; i++) {
             final Slide slide = Slide.createVerticalSlide(deque.pollFirst(), deque.pollLast());
             slides.add(slide);
         }
-        slides.addAll(horizontals.stream().map(Slide::createHorizontalSlide).collect(Collectors.toList()));
+
+        final List<Slide> horizontalSlides = photos.stream().filter(Photo::isHorizontal).map(Slide::createHorizontalSlide).collect(Collectors.toList());
+        slides.addAll(horizontalSlides);
+
         return slides;
     }
 
