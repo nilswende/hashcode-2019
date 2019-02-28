9d9719c319a8a62eeb0e2576b4d9ba79a6cb7c20
Karsten Koehler <karsten.koehler95@gmail.com>
2019-02-28 19:25:49 +0100

    random distribution of photos and slides

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -2,24 +2,44 @@ package hashcode;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main (String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException {
         final File[] files = new File("res").listFiles();
         for (File file : files) {
             List<Photo> photos = Input.read(file);
 
-            // TODO
 
+            Slideshow show = randomDistribution(photos);
+
+            // TODO output show
 
             final String fileName = file.getName();
             // Output.writeOutput(objs, "out/" + fileName.substring(0, fileName.indexOf(".")) + ".out.txt");
         }
+    }
+
+    private static Slideshow randomDistribution(List<Photo> photos) {
+        Slideshow show = new Slideshow();
+        List<Photo> verticals = photos.stream().filter(Photo::isVertical).collect(Collectors.toList());
+        List<Photo> horizontals = photos.stream().filter(Photo::isHorizontal).collect(Collectors.toList());
+
+        Collections.shuffle(verticals);
+        Collections.shuffle(horizontals);
+
+        for (int i = 0; i < verticals.size() - 1; i++) {
+            show.add(Slide.createVerticalSlide(verticals.get(i), verticals.get(i+1)));
+        }
+
+        for (Photo photo: horizontals) {
+            show.add(Slide.createHorizontalSlide(photo));
+        }
+
+        Collections.shuffle(show.getSlides());
+        return show;
     }
 
 }
