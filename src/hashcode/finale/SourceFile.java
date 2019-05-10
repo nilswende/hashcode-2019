6f013cfc079adbf492b3976cb679089a07ce173e
kevin <kevinlackmann.kl@gmail.com>
2019-05-10 20:05:50 +0200

    dependsOn in sourcefile

--- src/hashcode/finale/SourceFile.java
+++ src/hashcode/finale/SourceFile.java
@@ -9,15 +9,20 @@ public class SourceFile {
     private final int compilationTime;
     private final int replicationTime;
     private final int deadline;
+    private final int score;
     private final List<SourceFile> dependencies;
+    private final List<SourceFile> dependsOn;
 
-    public SourceFile(String id, boolean target, int compilationTime, int replicationTime, int deadline) {
+
+    public SourceFile(String id, boolean target, int compilationTime, int replicationTime, int deadline, int score) {
         this.id = id;
         this.target = target;
         this.compilationTime = compilationTime;
         this.replicationTime = replicationTime;
         this.deadline = deadline;
+        this.score = score;
         this.dependencies = new ArrayList<>();
+        this.dependsOn = new ArrayList<>();
     }
 
     public String getId() {
@@ -38,6 +43,12 @@ public class SourceFile {
 
     public int getDeadline() {
         return deadline;
+    }
+
+    public int getScore(){return score};
+
+    public List<SourceFile> getDependsOn () {
+        return dependsOn;
     }
 
     public List<SourceFile> getDependencies() {
