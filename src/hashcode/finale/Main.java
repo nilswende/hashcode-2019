ff1aa3b511f0265a08840a57a2d76cdab28faa4e
nilswende <nils.wende@studium.fernuni-hagen.de>
2019-05-10 20:33:39 +0200

    members

--- src/hashcode/finale/Main.java
+++ src/hashcode/finale/Main.java
@@ -4,9 +4,12 @@ import hashcode.Output;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    private static List<Server> servers;
+    private static List<SourceFile> sourceFiles;
 
     public static void main(String[] args) throws FileNotFoundException {
         final File[] files = new File("res/finale").listFiles();
@@ -21,9 +24,23 @@ public class Main {
 //            System.out.println(filename + ": The score is: " + score);
 //            finalScore += score;
 
-            ArrayList<Server> servers = new ArrayList<>();
             Output.writeServerOutput(servers, "out/finale/" + filename + ".out.txt");
         }
     }
 
+    public static List<Server> getServers () {
+        return servers;
+    }
+
+    public static void setServers (List<Server> servers) {
+        Main.servers = servers;
+    }
+
+    public static List<SourceFile> getSourceFiles () {
+        return sourceFiles;
+    }
+
+    public static void setSourceFiles (List<SourceFile> sourceFiles) {
+        Main.sourceFiles = sourceFiles;
+    }
 }
