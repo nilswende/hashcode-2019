839b0c3f0a2d946188fe68d7f7a027e5bd9d2038
kevin <kevinlackmann.kl@gmail.com>
2019-05-10 20:25:46 +0200

    rename

--- src/hashcode/finale/SourceFile.java
+++ src/hashcode/finale/SourceFile.java
@@ -4,54 +4,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SourceFile {
-    private final String id;
-    private final boolean target;
-    private final int compilationTime;
-    private final int replicationTime;
-    private final int deadline;
-    private final int score;
-    private final List<SourceFile> dependencies;
-    private final List<SourceFile> dependsOn;
+    private String id;
+    private boolean target;
+    private int compilationTime;
+    private int replicationTime;
+    private int deadline;
+    private int score;
+    private List<SourceFile> dependencies;
+    private List<SourceFile> isRequiredBy;
 
 
-    public SourceFile(String id, boolean target, int compilationTime, int replicationTime, int deadline, int score) {
-        this.id = id;
-        this.target = target;
+    public SourceFile (String id, int compilationTime, int replicationTime, List<SourceFile> dependencies) {
         this.compilationTime = compilationTime;
         this.replicationTime = replicationTime;
-        this.deadline = deadline;
-        this.score = score;
+        this.id = id;
+        this.dependencies = dependencies;
         this.dependencies = new ArrayList<>();
-        this.dependsOn = new ArrayList<>();
+        this.isRequiredBy = new ArrayList<>();
     }
 
-    public String getId() {
+
+    public String getId () {
         return id;
     }
 
-    public boolean isTarget() {
+    public boolean isTarget () {
         return target;
     }
 
-    public int getCompilationTime() {
+    public int getCompilationTime () {
         return compilationTime;
     }
 
-    public int getReplicationTime() {
+    public int getReplicationTime () {
         return replicationTime;
     }
 
-    public int getDeadline() {
+    public int getDeadline () {
         return deadline;
     }
 
-    public int getScore(){return score;}
+    public int getScore () {return score;}
 
-    public List<SourceFile> getDependsOn () {
-        return dependsOn;
+    public List<SourceFile> getIsRequiredBy () {
+        return isRequiredBy;
     }
 
-    public List<SourceFile> getDependencies() {
+    public void setIsTarget (boolean target) {
+        this.target = target;
+    }
+    public void addRequiredBy(SourceFile file){
+        isRequiredBy.add(file);
+    }
+
+    public List<SourceFile> getDependencies () {
         return dependencies;
     }
 }
